1f7e7a9c86a6479eab0d96133cd5e01a4bee699b
StepuroVadim <v.stepuroo@gmail.com>
2024-02-12 23:04:03 +0300

    Refactored servlet

--- src/main/java/org/example/servlet/GetWorkersServlet.java
+++ src/main/java/org/example/servlet/GetWorkersServlet.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.controller.WorkerController;
 import org.example.model.Worker;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,10 +21,12 @@ public class GetWorkersServlet extends HttpServlet {
     private final ObjectMapper mapper = new ObjectMapper();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        getAllWorkers(resp);
+    }
+
+    private void getAllWorkers(HttpServletResponse resp) throws IOException{
         List<Worker> workerList = controller.getAll();
-
         resp.setContentType("application/json");
-
 
         try{
             String json = mapper.writeValueAsString(workerList);
@@ -35,8 +36,6 @@ public class GetWorkersServlet extends HttpServlet {
         }catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private String getBody(HttpServletRequest req) throws IOException {
@@ -52,16 +51,15 @@ public class GetWorkersServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException{
+        createWorker(req, response);
+    }
 
+    private void createWorker(HttpServletRequest req, HttpServletResponse response)throws IOException{
         String json = getBody(req);
-
-
-
         Worker worker = mapper.readValue(json, Worker.class);
-
         Worker save = controller.save(worker);
-
         response.setContentType("application/json");
+
         try{
             json = mapper.writeValueAsString(save);
             PrintWriter writer = response.getWriter();
@@ -74,16 +72,15 @@ public class GetWorkersServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        editWorker(req, response);
+    }
 
+    private void editWorker(HttpServletRequest req, HttpServletResponse response) throws IOException {
         String json = getBody(req);
-
-
-
         Worker worker = mapper.readValue(json, Worker.class);
-
         Worker edit = controller.edit(worker);
-
         response.setContentType("application/json");
+
         try{
             json = mapper.writeValueAsString(edit);
             PrintWriter writer = response.getWriter();
@@ -96,9 +93,14 @@ public class GetWorkersServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        deleteWorker(req, response);
+    }
+
+    private void deleteWorker(HttpServletRequest req, HttpServletResponse response) throws IOException {
         int id =Integer.parseInt(req.getParameter("id"));
         controller.delete(id);
         response.setContentType("application/json");
+
         try{
             PrintWriter writer = response.getWriter();
             writer.println("");
